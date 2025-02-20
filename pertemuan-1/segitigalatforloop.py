673b3d5152be5c07e9ad542835f7749c372d4b4c
luthfijasirq <luthfiquthb@gmail.com>
2025-02-20 14:08:07 +0700

    Create segitigalatforloop.py

---
+++ pertemuan-1/segitigalatforloop.py
@@ -0,0 +1,7 @@
+nilai_maksimum = int(input("Masukkan maksimum input: "))
+
+for i in range(nilai_maksimum, 0, -1):
+    print(str(i) * i)
+
+for i in range(2, nilai_maksimum + 1):
+    print(str(i) * i)
